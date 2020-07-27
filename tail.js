2048e10d155e87f091da5ceae930a49e3c49541d
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-07-27 17:26:20 +0000

    lotide Project - tail feature refactor

--- tail.js
+++ tail.js
@@ -1,13 +1,3 @@
-const assertEqual = (actual, expected) => {
-  const nay = String.fromCodePoint(0x1F62D);
-  const yea = String.fromCodePoint(0x1F389);
-  if (actual === expected) {
-    return `${yea}${yea}${yea} Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    return `${nay}${nay}${nay} Assertion Failed: ${actual} !== ${expected}`;
-  }
-};
-
 const tail = args => {
   let result = [];
   if (args === [] || args.length === 1) {
@@ -18,19 +8,4 @@ const tail = args => {
   }
 };
 
-const test = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(test.length, 2)); // ensure we get back two elements
-console.log(assertEqual(test[0], "Lighthouse")); // ensure first element is "Lighthouse"
-console.log(assertEqual(test[1], "Labs"));
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
-
-const test2 = tail([3]);
-const test3 = tail([]);
-
-console.log(assertEqual(test2.length, 1));
-console.log(assertEqual(test3.length, 1));
-console.log(test2);
-console.log(test3);
+module.exports = tail;
